a41eacf6a878623b4e6c53734a07413ad7523cd7
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-10-05 10:27:07 +0530

    que 7

---
+++ qtn7.js
@@ -0,0 +1,12 @@
+var length=2;
+var width=20;
+var area=length*width;
+console.log(area);
+
+if (area>20){ 
+    console.log("The area is large");
+    
+}else {
+    console.log("the area is smaller");
+    
+}
