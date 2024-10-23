10e9482da2b74dccc0b35fbd30820dd9acdc41cc
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-10-23 13:57:12 +0530

    que 14

---
+++ qtn14.js
@@ -0,0 +1,15 @@
+var time=1004;
+if(time>=0&&time<=11){
+    console.log("morning");
+    
+}
+else if(time>=13&&time<=17){
+    console.log("afternoon");
+    
+}else if(time>=18&&time<=23){
+    console.log("evening");
+    
+}else{
+    console.log("error");
+    
+}
