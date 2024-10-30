b8395de7c8baa3d5314d4284b653b5f81db961ac
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-10-30 12:10:39 +0530

    que 17

---
+++ qtn17.js
@@ -0,0 +1,11 @@
+let age = 0; 
+
+if (age >= 1 && age <= 10) {
+    console.log("Small");
+} else if (age >= 11 && age <= 50) {
+    console.log("Medium");
+} else if (age <= 51) {
+    console.log("Large");
+} else {
+    console.log("error");
+}
