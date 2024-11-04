1e1715ac2d01ec2aed8aea4fa1efeca2bcd06d54
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-11-04 13:01:40 +0530

    que 19

---
+++ qtn19.js
@@ -0,0 +1,14 @@
+var weather="sunny"
+if(weather=="sunny"){
+    console.log("wear suncreen");
+    
+}else if(weather=="rainy"){
+    console.log("Bring an umbrella");
+    
+}else if(weather=="cloudy"){
+    console.log("Wear a light jacke");
+    
+}else{
+    console.log("Check the forecast again");
+    
+}
