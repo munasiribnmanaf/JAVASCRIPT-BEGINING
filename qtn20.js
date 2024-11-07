cf881b3bef255b8ec535223f204de2084c7d4f92
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-11-07 10:35:12 +0530

    que 20

---
+++ qtn20.js
@@ -0,0 +1,11 @@
+var  traffic="sunny"
+if( traffic=="green"){
+    console.log("Go");
+    
+}else if( traffic=="yellow"){
+    console.log("Caution");
+    
+}else{
+    console.log("stop");
+    
+}
