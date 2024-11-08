bf7f65f4b490c01125786445a80dd83c18b530ec
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-11-08 11:55:29 +0530

    que 21

---
+++ qtn21.js
@@ -0,0 +1,14 @@
+var day="sunday"
+if(day=="monday"){
+    console.log("Back to work");
+    
+}else if( day=="friday"){
+    console.log("Almost weekend");
+    
+}else if (day=="saturday" || day=="sunday"){
+    console.log("Weekend");
+    
+}else{
+    console.log("Just another day");
+    
+}
