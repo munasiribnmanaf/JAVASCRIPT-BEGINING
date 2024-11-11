1e9d8a131dab80b3da02f3167037ddeaf21212b6
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-11-11 10:18:11 +0530

    que 22

---
+++ qtn22.js
@@ -0,0 +1,17 @@
+var grade=70
+if(grade>=90 && grade<=100){
+    console.log("grade A");
+    
+}else if(grade>=80 && grade<=89){
+    console.log("grade B");
+    
+}else if(grade>=70 && grade<=80){
+    console.log("grade C");
+    
+}else if(grade>=60 && grade<=70){
+    console.log("grade D");
+    
+}else if(grade>=0 && grade<=60){
+    console.log("grade E");
+    
+}
