fedf10f985ac33454db7adade94ced5e626a4f24
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-11-13 10:27:31 +0530

    que 24

---
+++ qtn24.js
@@ -0,0 +1,13 @@
+
+for(i=1;i<=10;i++){
+      console.log("*");
+}
+//     var a='';
+//     for(i=1;i<=5;i++){ 
+//          a=a+'* ';  
+         
+//     }
+//      console.log(a); 
+    
+
+        
