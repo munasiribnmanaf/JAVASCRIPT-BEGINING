77f1f8a364478d673250ffc50a662a04c6afeb03
munasiribnmanaf <munasiribnumanaf@gmail.com>
2024-11-19 10:07:46 +0530

    que 26

---
+++ qtn26.js
@@ -0,0 +1,27 @@
+
+  for( i=1;i<=5;i++){
+     a="";
+  
+    for( k=1;k<=5-i;k++){
+      a+=" ";
+    }
+    for(m=1;m<=i;m++){
+        a+="* ";
+       
+    }
+    console.log(a); 
+    
+    }
+
+  for(i=5;i>=1;i--){
+    a="";
+    for(m=1;m<=5-i;m++){
+      a+=" ";
+    }
+    for(j=1;j<=i;j++){
+      a+="* ";
+    }
+    console.log(a);
+    
+  }
+  
